89af2bfe964720df2e2192e69899fb47f6e8c238
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-12-21 21:56:42 +0300

    change example to moshi because hikari requires Java 11

--- examples/bukkit-java/src/main/java/com/example/ExamplePlugin.java
+++ examples/bukkit-java/src/main/java/com/example/ExamplePlugin.java
@@ -1,13 +1,13 @@
 package com.example;
 
-import com.zaxxer.hikari.HikariConfig;
+import com.squareup.moshi.Moshi;
 import revxrsal.zapper.ZapperJavaPlugin;
 
 public class ExamplePlugin extends ZapperJavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println(HikariConfig.class);
+        System.out.println(Moshi.class);
     }
 
     @Override
