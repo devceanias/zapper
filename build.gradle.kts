63df1d856ea275e59ff140f5c24c673e37600ccb
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-12-21 21:50:41 +0300

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,15 +16,19 @@ plugins {
 }
 
 group = "io.github.revxrsal"
-version = "1.0.0"
+version = "1.0.1"
 
 subprojects {
+
+    val isExample = project.path.startsWith(":example")
 
     group = rootProject.group
     version = rootProject.version
 
     apply(plugin = "java")
-    apply(plugin = "com.vanniktech.maven.publish")
+
+    if (!isExample)
+        apply(plugin = "com.vanniktech.maven.publish")
 
     java {
         toolchain {
@@ -32,40 +36,42 @@ subprojects {
         }
     }
 
-    mavenPublishing {
-        coordinates(
-            groupId = group as String,
-            artifactId = "zapper.$name",
-            version = version as String
-        )
-        pom {
-            name.set("Zapper")
-            description.set("A powerful and flexible Maven dependency downloader at runtime")
-            inceptionYear.set("2024")
-            url.set("https://github.com/Revxrsal/Zapper/")
-            licenses {
-                license {
-                    name.set("MIT")
-                    url.set("https://mit-license.org/")
-                    distribution.set("https://mit-license.org/")
-                }
-            }
-            developers {
-                developer {
-                    id.set("revxrsal")
-                    name.set("Revxrsal")
-                    url.set("https://github.com/Revxrsal/")
-                }
-            }
-            scm {
+    if (!isExample) {
+        mavenPublishing {
+            coordinates(
+                groupId = group as String,
+                artifactId = "zapper.$name",
+                version = version as String
+            )
+            pom {
+                name.set("Zapper")
+                description.set("A powerful and flexible Maven dependency downloader at runtime")
+                inceptionYear.set("2024")
                 url.set("https://github.com/Revxrsal/Zapper/")
-                connection.set("scm:git:git://github.com/Revxrsal/Zapper.git")
-                developerConnection.set("scm:git:ssh://git@github.com/Revxrsal/Zapper.git")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://mit-license.org/")
+                        distribution.set("https://mit-license.org/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("revxrsal")
+                        name.set("Revxrsal")
+                        url.set("https://github.com/Revxrsal/")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/Revxrsal/Zapper/")
+                    connection.set("scm:git:git://github.com/Revxrsal/Zapper.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/Revxrsal/Zapper.git")
+                }
             }
-        }
 
-        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-        signAllPublications()
+            publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+            signAllPublications()
+        }
     }
 }
 
