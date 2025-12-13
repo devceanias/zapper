137f2368c3be9600150eb292fe82ef2a6c6d13a8
Plant <mythicalplanetdude@gmail.com>
2025-12-13 12:19:37 +1100

    fix publishPlugins error

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     id("com.gradle.plugin-publish") version "1.2.1"
 }
 
+group = rootProject.group
+version = rootProject.version
+
 repositories {
     mavenCentral()
     gradlePluginPortal()
